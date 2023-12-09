3f7349202e3d9f91527a11d555e3d6f5401a37b6
Chaitanya Aaditya <chaitanya.aaditya@procol.in>
2023-12-10 03:09:52 +0530

    remove footer

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Link from "next/link";
 import { hardhat } from "viem/chains";
-import { CurrencyDollarIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { CurrencyDollarIcon } from "@heroicons/react/24/outline";
 // import { HeartIcon } from "@heroicons/react/24/outline";
 import { SwitchTheme } from "~~/components/SwitchTheme";
 // import { BuidlGuidlLogo } from "~~/components/assets/BuidlGuidlLogo";
-import { Faucet } from "~~/components/scaffold-eth";
 import { useTargetNetwork } from "~~/hooks/scaffold-eth/useTargetNetwork";
 import { useGlobalState } from "~~/services/store/store";
 
@@ -30,7 +28,7 @@ export const Footer = () => {
                 </div>
               </div>
             )}
-            {isLocalNetwork && (
+            {/* {isLocalNetwork && (
               <>
                 <Faucet />
                 <Link href="/blockexplorer" passHref className="btn btn-primary btn-sm font-normal normal-case gap-1">
@@ -38,7 +36,7 @@ export const Footer = () => {
                   <span>Block Explorer</span>
                 </Link>
               </>
-            )}
+            )} */}
           </div>
           <SwitchTheme className={`pointer-events-auto ${isLocalNetwork ? "self-end md:self-auto" : ""}`} />
         </div>
